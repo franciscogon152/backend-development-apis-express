a6e333babb1f85128c7373ab2db8291ceef951a8
Francisco <frangon152@gmail.com>
2025-10-21 14:20:51 -0300

    /views/index.html

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
 console.log("Hello, World!");
-app.get ("/", (req, res,) => {
+/*app.get ("/", (req, res,) => {
   res.send("Hello Express");
+});*/
+
+app.get("/", function (req, res){
+    res.senFile(__dirname + "/views/index.html");
 });
 
 
