74b90c9abc8c8b0a2c759c3e7cbe27804deb36d2
Francisco <frangon152@gmail.com>
2025-10-22 19:10:32 -0300

    get /json

--- myApp.js
+++ myApp.js
@@ -11,7 +11,9 @@ app.get("/", function (req, res){
 
 app.use("/public", express.static(__dirname + "/public"));
 
-
+app.get("/json", function (req, res){
+    res.json({"message": "Hello json"})
+});
 
 
 
