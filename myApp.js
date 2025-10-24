f3ca81c7f82f6ee87f95d6cb4be6e629e282a236
Francisco <frangon152@gmail.com>
2025-10-24 15:35:01 -0300

    dotenv

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 console.log("Hello, World!");
@@ -13,7 +14,13 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", function (req, res){
     res.json({"message": "Hello json"})
+
+    if (process.env.MASSAGE_STYLE === "uppercase") {
+       massage = massage.toUpperCase();
+    }
+     res.json({"message": "HELLO JSON"})
 });
+
 
 
 
