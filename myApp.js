da01e479d00509d1806cd16066a4b6e75970828e
Francisco <frangon152@gmail.com>
2025-10-24 20:59:21 -0300

    add /now

--- myApp.js
+++ myApp.js
@@ -26,7 +26,14 @@ app.get("/json", function (req, res){
      res.json({"message": message});
 });
 
-
+app.get("/now", function (req, res, next){
+    req.time = new Date().toString();
+    next(); 
+    },
+    function (req, res){
+    res.json({time: req.time});
+    }
+);
 
 
 
